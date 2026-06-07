b600b47a8d1efd07200685d39753f14607ef8dc5
ShiganLuo <2530320102@qq.com>
2026-06-07 23:11:34 +0800

    fix:优化线程竞争

--- src/SV/run.sh
+++ src/SV/run.sh
@@ -94,13 +94,31 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 ### OncoPrint
 
-python ${SCRIPT_DIR}/run_OncoPrint.py \
-    -g PlaB06_vs_DMSO06:/data/pub/zhousha/Totipotent20251031/PacBio/SV/PlaB06_vs_DMSO06/PlaB06_vs_DMSO06_annotated.tab \
-    -g PlaB20_vs_PlaB06:/data/pub/zhousha/Totipotent20251031/PacBio/SV/PlaB20_vs_PlaB06/PlaB20_vs_PlaB06_annotated.tab \
-    -g DMSO20_vs_DMSO06:/data/pub/zhousha/Totipotent20251031/PacBio/SV/DMSO20_vs_DMSO06/DMSO20_vs_DMSO06_annotated.tab \
-    -g PlaB20_vs_DMSO20:/data/pub/zhousha/Totipotent20251031/PacBio/SV/PlaB20_vs_DMSO20/PlaB20_vs_DMSO20_annotated.tab \
-    -d PlaB06:/data/pub/zhousha/Totipotent20251031/data/Pacbio/RNAseq/P6.tsv \
-    -d PlaB20:/data/pub/zhousha/Totipotent20251031/data/Pacbio/RNAseq/P20.tsv \
-    --cosmic_file /data/pub/zhousha/Reference/human/Cosmic_CancerGeneCensusHallmarksOfCancer_v103_GRCh38.tsv \
-    -o /data/pub/zhousha/Totipotent20251031/PacBio/OncoPrint \
-    -f pdf
+# python ${SCRIPT_DIR}/run_OncoPrint.py \
+#     -g PlaB06_vs_DMSO06:/data/pub/zhousha/Totipotent20251031/PacBio/SV/PlaB06_vs_DMSO06/PlaB06_vs_DMSO06_annotated.tab \
+#     -g PlaB20_vs_PlaB06:/data/pub/zhousha/Totipotent20251031/PacBio/SV/PlaB20_vs_PlaB06/PlaB20_vs_PlaB06_annotated.tab \
+#     -g DMSO20_vs_DMSO06:/data/pub/zhousha/Totipotent20251031/PacBio/SV/DMSO20_vs_DMSO06/DMSO20_vs_DMSO06_annotated.tab \
+#     -g PlaB20_vs_DMSO20:/data/pub/zhousha/Totipotent20251031/PacBio/SV/PlaB20_vs_DMSO20/PlaB20_vs_DMSO20_annotated.tab \
+#     -d PlaB06:/data/pub/zhousha/Totipotent20251031/data/Pacbio/RNAseq/P6.tsv \
+#     -d PlaB20:/data/pub/zhousha/Totipotent20251031/data/Pacbio/RNAseq/P20.tsv \
+#     --cosmic_file /data/pub/zhousha/Reference/human/Cosmic_CancerGeneCensusHallmarksOfCancer_v103_GRCh38.tsv \
+#     -o /data/pub/zhousha/Totipotent20251031/PacBio/OncoPrint \
+#     -f pdf
+
+### gene model plot
+
+# python ${SCRIPT_DIR}/utils/gene_model.py \
+#     -t /data/pub/zhousha/Reference/mouse/GENCODE/GRCm39/gencode.vM38.primary_assembly.basic.annotation.gtf \
+#     -g Alk -g Ciita -g Fgfr2 -g Fhit -g Lrp1b \
+#     -m /data/pub/zhousha/Totipotent20251031/PacBio/SV/PlaB06_vs_DMSO06/PlaB06_vs_DMSO06_only.vcf \
+#     -o /data/pub/zhousha/Totipotent20251031/PacBio/SV/PlaB06_vs_DMSO06/gene \
+#     -f pdf \
+#     -j 5
+
+python ${SCRIPT_DIR}/utils/gene_model.py \
+    -t /data/pub/zhousha/Reference/mouse/GENCODE/GRCm39/gencode.vM38.primary_assembly.basic.annotation.gtf \
+    -g Alk -g Ciita -g Fgfr2 -g Fhit -g Lrp1b \
+    -m /data/pub/zhousha/Totipotent20251031/PacBio/SV/PlaB20_vs_DMSO20/PlaB20_vs_DMSO20_only.vcf \
+    -o /data/pub/zhousha/Totipotent20251031/PacBio/SV/PlaB20_vs_DMSO20/gene \
+    -f pdf \
+    -j 5

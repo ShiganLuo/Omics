7573be0304048d18cf7fb8e0833f9e61999c8d38
ShiganLuo <2530320102@qq.com>
2026-08-14 16:27:20 +0800

    fix:修改说明

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ python ${SCRIPT_DIR}/run.py \
     --singularity-args '--bind /home/luosg/Data/genomeStability/data/4RNAseq/fastq', \
     --rerun-triggers mtime \
     --Params.function.gmt /home/luosg/Data/genomeStability/workflow/Omics/assests/geneset/GSI_huam.gmt \
-    --forcerun function_gsea
+    --dry-run
 
 # python ${SCRIPT_DIR}/run.py \
 #     -m /home/luosg/Data/genomeStability/data/Srp54/meta_input.tsv \
